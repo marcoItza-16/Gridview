4b2a3595dc5c9389aebdb0a031cb226ff22a5f2c
marcoestrada-17 <javier.itza.est@qroo.nuevaescuela.mx>
2022-03-07 07:36:34 -0500

    ListGrid personalizado

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //ListGrid basico
+        //ListGrid personalizado
         var frutas:ArrayList<Fruta> = ArrayList()
         frutas.add(Fruta("Manzana", R.drawable.manzana))
         frutas.add(Fruta("Durazno", R.drawable.durazno))
